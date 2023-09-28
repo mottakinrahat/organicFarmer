1fb428716211e80ee80dc54537060a9e27ea4cb8
mottakinrahat <rahatchowhdury661@gmail.com>
2023-09-28 16:17:36 +0600

    liveLink

--- src/ProfileDashboard/Crops/Crops.jsx
+++ src/ProfileDashboard/Crops/Crops.jsx
@@ -9,7 +9,7 @@ const Crops = () => {
     const [arrayData, setArrayData] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/personalInfo')
+        fetch('https://organic-farmers-server.vercel.app/personalInfo')
             .then(res => res.json())
             .then(data => setArrayData(data))
     }, [])
@@ -30,7 +30,7 @@ const Crops = () => {
 
     //     const cropsData = { productName, ProductImage, quantity, unit, price,amount, Variety, email: user?.email, name: user?.displayName, number, location }
     //     console.log(cropsData);
-    //     fetch('http://localhost:5000/crops', {
+    //     fetch('https://organic-farmers-server.vercel.app/crops', {
     //         method: 'POST',
     //         headers: { 'Content-Type': 'application/json' },
     //         body: JSON.stringify(cropsData)
@@ -67,7 +67,7 @@ const Crops = () => {
                     const {productName, quantity,price,Variety,unit,amount} = data;
                     const cropsInfo = { productName, quantity,unit,price,amount,Variety, ProductImage: imageUrl,email:user.email,name:user.displayName}
 
-                    fetch('http://localhost:5000/crops', {
+                    fetch('https://organic-farmers-server.vercel.app/crops', {
                         method: 'POST',
                         headers: { 'Content-Type': 'application/json' },
                         body: JSON.stringify(cropsInfo)
--- src/component/ProfileRoute/ProfileRoute.jsx
+++ src/component/ProfileRoute/ProfileRoute.jsx
@@ -13,7 +13,7 @@ const ProfileRoute = () => {
     const [cropsData, setCropsData] = useState([])
     const userData = useLoaderData();
     useEffect(() => {
-        fetch('http://localhost:5000/crops')
+        fetch('https://organic-farmers-server.vercel.app/crops')
             .then(res => res.json())
             .then(data => {
                 const filteredData = data.filter(item => item.email === user?.email);
@@ -31,7 +31,7 @@ const ProfileRoute = () => {
             confirmButtonText: 'Yes, delete it!'
         }).then((result) => {
             if (result.isConfirmed) {
-                fetch(`http://localhost:5000/crops/${id}`, {
+                fetch(`https://organic-farmers-server.vercel.app/crops/${id}`, {
                     method: 'DELETE'
                 })
                     .then((res) => res.json())
