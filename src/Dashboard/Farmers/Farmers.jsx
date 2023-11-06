723c4b65871725a9b5d74ca4feda4d55586ae462
mottakinrahat <rahatchowhdury661@gmail.com>
2023-11-06 23:45:25 +0600

    'added

--- src/Dashboard/Farmers/Farmers.jsx
+++ src/Dashboard/Farmers/Farmers.jsx
@@ -17,13 +17,13 @@ const Farmers = () => {
    
 
     return (
-        <div className='mx-[25px]'>
+        <div className='mx-[20px]'>
             <div className='md:flex justify-between  text-center pt-[38px]'>
                 <div><h2 className='text-[32px] font-semibold'>Our Farmers</h2></div>
                 {/* <div className='border border-black rounded-full flex items-center bg-white'><span className='mx-4'><FaSearch /></span><input type="text" placeholder="Type here" className="input h-[43px] rounded-r-full w-80 max-w-xs" /></div> */}
             </div>
 
-            <div className='md:grid grid-cols-3 gap-4 my-[101px] md:px-20'>
+            <div className='md:grid grid-cols-3 gap-4 my-[101px] md:px-10'>
                 {
                     arrayData.map((sData) => (
                         <SingleFarmers key={sData._id} sData={sData}></SingleFarmers>
--- src/Dashboard/Traders/Traders.jsx
+++ src/Dashboard/Traders/Traders.jsx
@@ -15,13 +15,13 @@ const Traders = () => {
     }, []);
 
     return (
-        <div className='mx-[25px]'>
+        <div className='mx-[20px]'>
             <div className='flex justify-between pt-[32px]'>
                 <div><h2 className='text-[32px] font-semibold'>Our Traders</h2></div>
                 {/* <div className='border hidden border-black rounded-full  items-center bg-white'><span className='mx-4 '><FaSearch /></span><input type="text" placeholder="Type here" className="input h-[43px] rounded-r-full w-80 max-w-xs" /></div> */}
             </div>
 
-            <div className='grid grid-cols-3 gap-4 my-[101px] md:px-20 '>
+            <div className='grid grid-cols-3 gap-4 my-[101px] md:px-10 '>
                 {
                     arrayData.map((sData, index) => (
                         <SingleTraders key={index} sData={sData}></SingleTraders>
